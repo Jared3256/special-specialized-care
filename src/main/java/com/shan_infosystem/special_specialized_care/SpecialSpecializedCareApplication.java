1ae3d9b7122ad1b25853b4536c045cbf499e9e35
Jared3256 <odhiambojared566@gmail.com>
2024-05-23 13:10:27 +0300

    first comit

---
+++ src/main/java/com/shan_infosystem/special_specialized_care/SpecialSpecializedCareApplication.java
@@ -0,0 +1,13 @@
+package com.shan_infosystem.special_specialized_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialSpecializedCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialSpecializedCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shan_infosystem/special_specialized_care/SpecialSpecializedCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan_infosystem.special_specialized_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialSpecializedCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
