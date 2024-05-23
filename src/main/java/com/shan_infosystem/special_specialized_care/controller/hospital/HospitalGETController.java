03817a7040465c6b2d3f7148ffe840a29eb1552b
Jared3256 <odhiambojared566@gmail.com>
2024-05-23 13:49:17 +0300

    Added Hospital Controller class

---
+++ src/main/java/com/shan_infosystem/special_specialized_care/controller/hospital/HospitalGETController.java
@@ -0,0 +1,11 @@
+package com.shan_infosystem.special_specialized_care.controller.hospital;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/hsp/g")
+public class HospitalGETController
+{
+    private static final Logger logger = LoggerFactory.getLogger(HospitalGETController.class);
+}
